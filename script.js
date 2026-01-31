cb2eddc68b1e602cb4936d2afb521d41269f627e
jwwilson1993 <jwwilson1993@gmail.com>
2026-01-30 20:13:41 -0800

    Add script to scroll view projects button.

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.getElementById("view-projects").addEventListener("click", () => {
+  document.getElementById("projects").scrollIntoView({
+    behavior: "smooth"
+  });
+});
+
+const button = document.getElementById("view-projects");
+
